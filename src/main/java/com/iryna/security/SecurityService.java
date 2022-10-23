889e283b55a0e9489fa7577a017362c89eccf4b7
ISotska <iryna.sotska@gettyimages.com>
2022-10-23 23:31:31 +0300

    fix date time formatter

--- src/main/java/com/iryna/security/SecurityService.java
+++ src/main/java/com/iryna/security/SecurityService.java
@@ -6,12 +6,14 @@ import com.iryna.service.UserService;
 import com.iryna.util.ConfigLoader;
 import com.iryna.util.PasswordEncryptor;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDateTime;
 import java.util.*;
 
 import static com.iryna.security.entity.Role.*;
 
+@Slf4j
 @RequiredArgsConstructor
 public class SecurityService {
 
